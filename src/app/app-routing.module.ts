6a5014574c9051e0abd5b28e23d935cbe9c12d2b
silviucostin44 <58137480+silviucostin44@users.noreply.github.com>
2021-11-01 22:15:21 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {QuestionnaireComponent} from './pages/questionnaire/questionnaire.component';
+import {ProgramComponent} from './pages/program/program.component';
+import {SelfAssessmentComponent} from './pages/self-assessment/self-assessment.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'questionnaire/:id', component: QuestionnaireComponent},
+  {path: 'questionnaire/:q_id/self-assessment/:id', component: SelfAssessmentComponent},
+  {path: 'program/:id', component: ProgramComponent},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './pages/home/home.component';
+import {QuestionnaireComponent} from './pages/questionnaire/questionnaire.component';
+import {ProgramComponent} from './pages/program/program.component';
+import {SelfAssessmentComponent} from './pages/self-assessment/self-assessment.component';
+import {PageWrapperComponent} from './abstract/page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    QuestionnaireComponent,
+    ProgramComponent,
+    SelfAssessmentComponent,
+    PageWrapperComponent,
   ],
   imports: [
     BrowserModule,
